48b237223050a071e75969da787e1a46d49c9462
hakimkt <hakimkt@gmail.com>
2023-03-02 23:21:47 +0300

    Update csrf_scanner.py

--- csrf_scanner.py
+++ csrf_scanner.py
@@ -1,40 +1,39 @@
-import argparse
-import requests
-from bs4 import BeautifulSoup
+import sys
+import re
+import httplib2
 
-def check_csrf(url):
-    # Send a GET request to the URL
-    response = requests.get(url)
+# Check if URL is provided as command line argument
+if len(sys.argv) < 2:
+    print("Usage: python3 csrf_scanner.py <url>")
+    sys.exit(1)
 
-    # Check if the request was successful
-    if response.status_code != 200:
-        print("Error: Unable to access the URL")
-        return
+# Retrieve URL from command line argument
+url = sys.argv[1]
 
-    # Parse the HTML content of the webpage using BeautifulSoup
-    soup = BeautifulSoup(response.content, 'html.parser')
+# Create HTTP client
+http = httplib2.Http()
 
-    # Find all forms in the HTML code
-    forms = soup.find_all('form')
+# Retrieve HTML content from URL
+response, content = http.request(url)
 
-    # Check each form for CSRF protection
-    for form in forms:
-        # Check if the form contains a CSRF token
-        csrf_token = form.find('input', {'name': 'csrf_token'})
-        if csrf_token is None:
-            # CSRF token not found, possible CSRF attack
-            print(f"Possible CSRF attack detected in form {form}")
-        else:
-            # CSRF token found, check its value
-            if csrf_token.get('value') == '':
-                # Empty CSRF token value, possible CSRF attack
-                print(f"Possible CSRF attack detected in form {form}")
+# Extract all forms from HTML content
+forms = re.findall(r'<form.*?>(.*?)</form>', content.decode('utf-8'), re.DOTALL)
 
-if __name__ == '__main__':
-    # Parse the command-line arguments
-    parser = argparse.ArgumentParser(description='Scan a URL for possible CSRF attacks')
-    parser.add_argument('url', type=str, help='The URL to be scanned')
-    args = parser.parse_args()
+# Flag to indicate whether a CSRF vulnerability was found
+csrf_found = False
 
-    # Call the check_csrf function with the provided URL
-    check_csrf(args.url)
+# Check each form for potential CSRF vulnerability
+for form in forms:
+    # Check if form contains a CSRF token field
+    if re.search(r'<input.*?name=["\']csrf_token["\']', form):
+        # Check if form does not have a method attribute or if it uses POST
+        if not re.search(r'method=["\'].*?["\']', form) or re.search(r'method=["\']post["\']', form):
+            # Check if form action URL is different from current URL
+            if not re.search(r'action=["\']{}["\']'.format(url), form):
+                print("Potential CSRF vulnerability found:")
+                print(form)
+                csrf_found = True
+
+# If no CSRF vulnerabilities were found, notify the user
+if not csrf_found:
+    print("No CSRF vulnerabilities found.")
